cc9d31828593a8e4071b187954183c38249ffaad
asimmaqsood57 <asimmaqsood57@gmail.com>
2021-09-28 17:35:51 +0500

    some files are modified

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div className="container">
+      <button className="btn btn-primary">-</button>
+
+      <p>Add this item to cart</p>
+      <button className="btn btn-primary">+</button>
+    </div>
+  );
+};
+
+export default Shop;
