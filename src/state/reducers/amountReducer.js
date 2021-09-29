368bb1e1a6c34dbcdb3073a108b9642c19eb27d5
asimmaqsood57 <asimmaqsood57@gmail.com>
2021-09-29 17:24:59 +0500

    redux store is added

--- src/state/reducers/amountReducer.js
+++ src/state/reducers/amountReducer.js
@@ -1,10 +1,12 @@
-export default reducer = (state = 0, action) => {
-  if (action.type == "deposit") {
+const reducer1 = (state = 0, action) => {
+  if (action.type === "deposit") {
     return state + action.payload;
   }
-  if (action.type == "withdraw") {
+  if (action.type === "withdraw") {
     return state - action.payload;
   } else {
     return state;
   }
 };
+
+export default reducer1;
